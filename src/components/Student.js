71377ec26628a99fee4adf4d951e2bc96f33e405
jasoncfleeBCIT <59238032+jasoncfleeBCIT@users.noreply.github.com>
2021-06-22 11:59:49 -0700

    part 2 done

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,10 +1,36 @@
+//This function takes an array of grades and returns the average
+function averageGrade(gradeArray){
+    let sum = 0; 
+    for (let position = 0; position < gradeArray.length; position++){
+        sum = sum + parseInt(gradeArray[position]);
+    }
+    return sum / gradeArray.length;
+}
+
 const Student = ({student}) => {
     return (
         <div className='student'>
-            <h1>{student.city}</h1>
+            <div className='studentLeft'>
+                <div className='studentPictureContainer'>
+                    <img className='studentPicture' src={student.pic} alt=""></img>
+                </div>
+            </div>
+            <div className='studentRight'>
+                <div className='studentNameContainer'>
+                    <label>{student.firstName.toUpperCase()} {student.lastName.toUpperCase()}</label>
+                </div>
+                <div className='studentDetailContainer'>
+                    <p className='studentDetail'>Email: {student.email}</p>
+                    <p className='studentDetail'>Company: {student.company}</p>
+                    <p className='studentDetail'>Skill: {student.skill}</p>
+                    <p className='studentDetail'>Average: {averageGrade(student.grades)}%</p>
+                </div>
+            </div>
+            
+            
+            
         </div>
     )
 }
-
 export default Student
 
--- src/components/Students.js
+++ src/components/Students.js
@@ -1,34 +1,12 @@
-// import React, {useEffect, useState} from "react"
-// import axios from "axios"
 import Student from './Student'
 
 const Students = ({students}) => {
 
-    // useEffect(() =>{
-    //     getStudents();
-    // }, [])
-
-    // const [students, setStudents] = useState([])
-
-    // const getStudents = async() => {
-    //     try{
-    //         const response = await axios.get("https://api.hatchways.io/assessment/students");
-    //         setStudents(response.data.students);
-    //         console.log(response.data.students);
-    //     }catch(err){
-    //         alert(err.message);
-    //     }
-    // }
     return (
-        <div>
-            {/* {students.map((student)=>(
-                <h1 key={student.id}>{student.city}</h1>
-            ))} */}
-
+        <div className="studentContainer">
             {students.map((student)=>(
                 <Student key={student.id} student={student}/>
             ))}
-            
         </div>
     )
 }
