6666f0ceceaafebf6965778674b142d579de18be
jasoncfleeBCIT <59238032+jasoncfleeBCIT@users.noreply.github.com>
2021-06-23 19:30:49 -0700

    almost done part 4

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,3 +1,5 @@
+import React, {useState} from 'react';
+
 //This function takes an array of grades and returns the average
 function averageGrade(gradeArray){
     let sum = 0; 
@@ -7,7 +9,14 @@ function averageGrade(gradeArray){
     return sum / gradeArray.length;
 }
 
+function expandMe(gradeArray){
+
+}
+
 const Student = ({student}) => {
+
+    const [show, setShow] = useState(false);
+
     return (
         <div className='student'>
             <div className='studentLeft'>
@@ -15,7 +24,7 @@ const Student = ({student}) => {
                     <img className='studentPicture' src={student.pic} alt=""></img>
                 </div>
             </div>
-            <div className='studentRight'>
+            <div className='studentRight' id="test">
                 <div className='studentNameContainer'>
                     <label>{student.firstName.toUpperCase()} {student.lastName.toUpperCase()}</label>
                 </div>
@@ -25,9 +34,18 @@ const Student = ({student}) => {
                     <p className='studentDetail'>Skill: {student.skill}</p>
                     <p className='studentDetail'>Average: {averageGrade(student.grades)}%</p>
                 </div>
+                {
+                    show?<div className='studentGradeContainer'>
+                        {student.grades.map((studentGradeList, index)=>(
+                            <p className='studentDetail'>Test {index + 1}: {studentGradeList}%</p>
+                        ))}
+                    </div>:null
+                }
             </div>
-            <div className="buttonFarRight">
-                <button type="button">+</button>
+            <div className='buttonFarRight'>
+                <div className='buttonContainer'>
+                    <button className='buttonExpand' onClick={ () => setShow(!show)}>{show? "-" : "+"}</button>
+                </div>
             </div>
             
         </div>
